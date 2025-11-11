990d026691e93ac368dcedf7ff65ef630dd3895f
graef <simonjn45@gmail.com>
2025-11-11 12:09:05 +0100

    pkmn-day

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,2 +1,26 @@
-import { getDayTime } from './pkmn-day.js'
-document.body.classList.add(getDayTime(new Date(),true))
+import { getSeason, getDayTime } from './pkmn-day.js'
+const daytime = getDayTime(new Date(), true)
+const season  = getSeason(new Date(), true)
+document.body.classList.add(daytime)
+console.log(`${season}, ${daytime}`)
+
+/* getting the theme, stuff ... etc.
+ * very small localStorage wrapper. 
+ */
+class Theme {
+	static get current () {
+		if (localStorage !== undefined && localStorage.getItem("theme")) {
+			return localStorage.getItem("theme")
+		} else {
+			return undefined
+		}
+	}
+	static set current (value) {
+		localStorage.setItem("theme", value)
+	}
+	static delete() {
+		localStorage.remove("theme")
+	}
+}
+
+const theme = Theme.current
