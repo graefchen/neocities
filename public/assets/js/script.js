1b0d7075a2c1322e9ff7df3acaaad0cda3310f01
graef <simonjn45@gmail.com>
2026-02-07 12:50:13 +0100

    some misc changes

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,27 +1,28 @@
-import { getSeason, getDayTime } from './pkmn-day.js'
-const daytime = getDayTime(new Date(), true)
-const season  = getSeason(new Date(), true)
-document.body.classList.add(daytime)
-console.log(`${season}, ${daytime}`)
+import { getDayTime } from "./pkmn-day.js";
 
 /* getting the theme, stuff ... etc.
- * very small localStorage wrapper. 
+ * very small localStorage wrapper.
  */
 class Theme {
-  static get current () {
+  static get current() {
     if (localStorage !== undefined && localStorage.getItem("theme")) {
-      return localStorage.getItem("theme")
+      return localStorage.getItem("theme");
     } else {
-      return undefined
+      return undefined;
     }
   }
-  static set current (value) {
-    localStorage.setItem("theme", value)
+  static set current(value) {
+    localStorage.setItem("theme", value);
   }
   static delete() {
-    localStorage.remove("theme")
+    localStorage.remove("theme");
   }
 }
 
-Theme.current.set(getDayTime(new Date(), true))
-document.body.classList.add(Theme.current.get())
+// First adding the Theme, so we
+// know what Theme is used.
+Theme.current = getDayTime(new Date(), true);
+// Using the Theme to get the current theme.
+// And adding it to the class list of the body.
+if (Theme.current != undefined)
+  document.body.classList.add(Theme.current);
